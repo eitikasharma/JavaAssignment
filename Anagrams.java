c84d998e54794ec91c3dbdded3644fa2379e4de8
eitikasharma <44219225+eitikasharma@users.noreply.github.com>
2020-01-22 22:03:40 +0530

    Anagrams

---
+++ Anagrams.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagrams {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String s1=sc.next();
+    String s2=sc.next();
+    int count=0;
+    char []ch1=s1.toCharArray();
+    char []ch2=s2.toCharArray();
+    Arrays.sort(ch1);
+    Arrays.sort(ch2);
+        if (s1.length() == s2.length()) 
+        {
+            for (int i = 0; i < s1.length(); i++) 
+            {
+                    if(ch1[i]!=ch2[i])
+                    {
+                       break;
+                    }
+            }
+             System.out.println("Anagrams");
+        }
+        else 
+        {
+            System.out.println("Not Anagrams");
+        }
+}
+}
